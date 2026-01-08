76799e1bd68ac67a6b1ca125adbcb6e2a3c7d82f
Manoj Hajam <manojhajam4@gmail.com>
2026-01-08 17:17:21 +0545

    Created first Welcome Page

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native";
+
+const Index = () => {
+  return <View style={{
+    backgroundColor: "red",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  
+  }}>
+    <Text style={{fontSize: 30, fontWeight:"700", color:"white"}}>Hello Welcome to Coder</Text>
+  </View>;
+};
+
+export default Index;
