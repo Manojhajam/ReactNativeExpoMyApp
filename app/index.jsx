36305a22a717e058b2208834171ea9af10ee0996
Manoj Hajam <manojhajam4@gmail.com>
2026-01-10 11:25:52 +0545

    expo router to change routes

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,19 @@
-import { Link } from "expo-router";
-import { View, Text } from "react-native";
+import { Link, useRouter } from "expo-router";
+import { View, Text, TouchableOpacity } from "react-native";
 
 const Index = () => {
+  const router = useRouter();
   return <View className="bg-red-500" style={{
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
   
   }}>
-    <Text style={{ fontSize: 30, fontWeight: "700" }}>Hello Welcome to Coder</Text>
-    <Link href={"/about"}>Go to about</Link>
+    <Text className="text-blue-600" style={{ fontSize: 30, fontWeight: "700" }}>Hello Welcome to Coder</Text>
+    <Link className="text-sky-600" href={"/about"}>Go to about</Link>
+    <TouchableOpacity onPress={() => router.push("/about")}>
+      <Text>Change route</Text>
+    </TouchableOpacity>
   </View>;
 };
 
