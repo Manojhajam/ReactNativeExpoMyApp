14c56892286181e6287d81397cd21936e4a65d7c
Manoj Hajam <manojhajam4@gmail.com>
2026-01-10 11:09:10 +0545

    Nativewind setup: for tailwind CSS

--- app/index.jsx
+++ app/index.jsx
@@ -2,14 +2,13 @@ import { Link } from "expo-router";
 import { View, Text } from "react-native";
 
 const Index = () => {
-  return <View style={{
-    backgroundColor: "red",
+  return <View className="bg-red-500" style={{
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
   
   }}>
-    <Text style={{ fontSize: 30, fontWeight: "700", color: "white" }}>Hello Welcome to Coder</Text>
+    <Text style={{ fontSize: 30, fontWeight: "700" }}>Hello Welcome to Coder</Text>
     <Link href={"/about"}>Go to about</Link>
   </View>;
 };
